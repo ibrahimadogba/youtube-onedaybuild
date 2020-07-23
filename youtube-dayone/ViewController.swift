8d37bb861af53462f593f18d76ef59d97403ff08
Ibrahim Adogba <ibrahimadogba@Ibrahims-MacBook-Air.local>
2020-07-23 12:52:30 -0600

    initial xcode project creation

---
+++ youtube-dayone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-dayone
+//
+//  Created by Ibrahim Adogba on 2020-07-23.
+//  Copyright © 2020 Ibrahim Adogba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
